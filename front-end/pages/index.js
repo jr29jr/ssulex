c749c92a5461fd760e9e0dce135c2c0fb35e5a1b
jr29jr <jr30jr@naver.com>
2019-10-15 20:17:14 +0900

    화면구성
    기본틀은 완성,css 적용이 어려워서 미완성

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,5 +1,78 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>
+import Header from "../components/Header"
+import InfoLayout from "../components/InfoLayout"
+import { Head } from "next/document";
+
+const meta=require("../components/meta.js")
+
+let series=new Array();
+let list=[1,2,3,4]
+function makeList(){
+    let s=meta.getSeriesAll();
+    for(let id in s){
+        s[id].id=id;
+        series.push(s[id]);
+    }
+}
+function retu(){
+    let test=<div>
+        {list.map((value) =>(
+                <InfoLayout id={value.id} thumb_url={value.thumb_url} title={value}></InfoLayout>
+        ))}
+        <InfoLayout id='td'title='hello'></InfoLayout>
+        <InfoLayout id='td'title='hello'></InfoLayout>
+    </div>
+    return test;
 }
 
-export default Home
+function search(tt){
+	if(tt.key == 'Enter'){
+		let result=tt.currentTarget.value;
+		console.log(result);		
+	}
+}
+
+function filter(keyword) {
+	let ret = {};
+	let s=meta.getSeriesAll();
+    for (let id in s)
+        for (let value of Object.values(s[id]))
+            if (typeof value === "string") {
+                if (value.toLowerCase().includes(keyword.toLowerCase()))
+                    ret[id] = s[id];
+            }
+            else if (typeof value === 'object')
+                for (let e of value)
+                    if (e.toLowerCase().includes(keyword.toLowerCase()))
+                        ret[id] = s[id];
+    return ret;
+}
+
+const Index = () =>(
+	<div>
+        <Header/>
+        <div id="view" className="main">
+            {makeList()}
+            {series.map((value) =>(
+                <InfoLayout id={value.id} thumb_url={value.thumb_url} title={value.title}></InfoLayout>
+            ))}
+        </div>
+    <style jsx>{`
+    #top {
+        display: flex;
+        height: 50px;
+        background-color: #1d1d1d;
+    }
+    #top > input {
+        background-color: #3f3f3f;
+        color: white;
+        width: 15%;
+        margin: 10px 10px 10px auto;
+        border: 0px;
+        padding: 10px;
+        border-radius: 10px;
+    }
+    `}</style>
+	</div>
+)
+
+export default Index
