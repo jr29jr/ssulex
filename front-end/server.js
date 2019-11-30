d711d9c737c0327413c1a1df2dc293e47436fc2e
jr29jr <jr30jr@naver.com>
2019-12-01 00:19:53 +0900

    search,detail api 요청 수정
    결과값을 html이 아니라 html을 만들기 위한 데이터를 가진 json으로 수정

--- front-end/server.js
+++ front-end/server.js
@@ -8,72 +8,25 @@ const dev=process.env.NODE_ENV !== "production";
 const app=next({dev})
 const handle=app.getRequestHandler();
 
+
 app.prepare()
 .then(() => {
     const server= express()
 
-    server.get("/search",(req,res) => {
+    server.get("/api/search",(req,res) => {
+        let type=req.query.type;
         let keyword=req.query.keyword;
-        let info=meta.getSeriesByKeyword(keyword);
-        let list="";
-        for(let id in info){
-            list+=`<figure id=${id}>
-                <img src=${info[id]["thumb_url"]}/>
-                <figcaption>${info[id]["title"]}</figcaption>
-            </figure>`
-        }    
-        let output=`${list}`;
-        res.send(output);
+        console.log(type+"/"+keyword);
+        let result=meta.getSeriesByKeyword(type,keyword);
+        res.json(result);
     })
 
-    server.get("/detailSeries",(req,res) => {
+    server.get("/api/detailSeries",(req,res) => {
         let id=req.query.id;
-        let genre="";
-        let seriesInfo=meta.getSeries(id);
-        let epsIds=new Array();
-        if(seriesInfo != null){
-            seriesInfo["genres"].forEach(function(element, index) {
-                if (index != 0)
-                    genre+= ", ";
-                genre+= element;
-            });
-            let temp=meta.getEps(id);
-            if(temp != null)
-                temp.forEach(function(element,index){
-                    epsIds.push(element);
-                })    
-        }
-        let list="";
-        epsIds.forEach(function(element){
-            let info=meta.getDetailOfEp(element);
-            list+=
-            `<div className="epsInfo">
-                <div>
-                    <span id="title">${info["title"]}</span>
-                    <span> (S${info["season"]}, Ep${info["number"]})</span>
-                    <span id="date">${info["date"]}</span>
-                </div>
-                <div>
-                    <img src="../img/star.jpg"></img>
-                    <span> ${info["rating"]}</span>
-                </div>
-                <p>${info["description"]}</p>
-            </div>`;
-        });
-        let output=
-        `<div id="detailInfo">
-            <nav>
-                <img src=${seriesInfo["thumb_url"]}></img>
-                <p className="center">${seriesInfo["title"]}</p>
-                <p className="center">${seriesInfo["year"]}</p>
-                <p className="center">${genre}</p>
-                <p className="center">${seriesInfo["synopsis"]}</p>
-            </nav>
-            <article>
-                ${list}
-            </article>
-        </div>`;
-        res.send(output);
+        let seriseInfo=meta.getDataForDetailPage(id);
+        if(seriseInfo == null)
+            seriseInfo={};
+        res.json(seriseInfo);
     })
     server.get('*', (req, res) => {
         return handle(req, res)
